56fe2542116ea82df1356bd86ca6dd5abdab8f6d
Sammy Braun <sammy@atama.co>
2023-03-30 13:29:26 +0200

    feat: allow default exports in app directory

--- next.js
+++ next.js
@@ -2,30 +2,22 @@ const baseConfig = require('./base');
 
 module.exports = {
   ...baseConfig,
-  extends: [
-    'next',
-    ...baseConfig.extends,
-  ],
+  extends: ['next', ...baseConfig.extends],
   overrides: [
     ...baseConfig.overrides,
     // Next.js needs default exports for pages and API endpoints
     {
-      'files': [
-        'pages/**/*',
-        'src/pages/**/*'
-      ],
-      'rules': {
-        'import/no-default-export': 'off'
-      }
+      files: ['pages/**/*', 'src/pages/**/*', 'src/app/**/*'],
+      rules: {
+        'import/no-default-export': 'off',
+      },
     },
     // next.config.js is a build file hence "devDependencies" should be allowed.
     {
-      'files': [
-        'next.config.js'
-      ],
-      'rules': {
-        'import/no-extraneous-dependencies': 'off'
-      }
-    }
-  ]
-}
+      files: ['next.config.js'],
+      rules: {
+        'import/no-extraneous-dependencies': 'off',
+      },
+    },
+  ],
+};
